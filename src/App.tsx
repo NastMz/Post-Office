bc7c89a38c02f56670e14cb85b5cbd4e985a69f7
Kevin Martinez <ksmartinez2307@gmail.com>
2022-07-03 18:26:28 -0500

    Initial release

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import {Route, Routes} from 'react-router-dom';
 import './App.css';
+import React from "react";
+import {NotFound} from "./Pages/NotFound/NotFound";
+import {Paths} from "./Utils/Paths";
+import {store} from "./Redux/store";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    let paths: Array<any> = [];
+    Paths.forEach((path) => {
+        paths.push(<Route path={path.pathname} element={path.element} key={Math.random()}/>)
+    });
+
+    return (
+        <div className="App">
+            <Routes>
+                {/* 404 rounte */}
+                <Route path="*" element={<NotFound/>}/>
+                {paths.map((path) => path)}
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
+
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import './Assets/css/variables.css';
+import './Assets/fonts/fontawesome/fontawesome-free-6.1.1-web/css/all.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter} from "react-router-dom";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <BrowserRouter>
+        <App/>
+    </BrowserRouter>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
